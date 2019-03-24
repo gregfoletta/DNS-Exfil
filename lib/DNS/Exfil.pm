79bbbcdeda92b961724329f17a0786ec4dc04222
Greg Foletta <greg@foletta.org>
2019-03-24 14:09:37 +1100

    Initial commit

---
+++ lib/DNS/Exfil.pm
@@ -0,0 +1,27 @@
+package DNS::Exfil;
+
+use strict;
+use warnings;
+use 5.010;
+
+# VERSION
+# PODNAME
+# ABSTRACT: new module
+
+=encoding utf8
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 ERRORS 
+
+=head1 METHODS
+
+=head2 new
+
+=cut
+
+
+
+1;
